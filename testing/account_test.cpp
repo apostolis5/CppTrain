a8429c9f066cbe4f77925275dec5dd7e9ced5944
apostolis santas <apostolis.santas@voiceland.gr>
2026-01-16 15:22:41 +0200

    bank account testing ok

--- testing/account_test.cpp
+++ testing/account_test.cpp
@@ -8,15 +8,17 @@ using namespace std;
 TEST_CASE("BankAccount operations", "[account]") {
     BankAccount default_account;
     REQUIRE(default_account.get_balance() == 0);
-
     BankAccount account(4000);
     REQUIRE(account.get_balance() == 4000);
-
     account.deposit(300);
     REQUIRE(account.get_balance() == 4300);
-
     account.withdraw(500);
     REQUIRE(account.get_balance() == 3800);
+
+    BankAccount NewAccount;
+    NewAccount.deposit(300);
+    NewAccount.withdraw(200);
+    REQUIRE(NewAccount.get_balance() == 100);
 }
 
 // This test case CHECKS FOR A FAILURE.
@@ -26,8 +28,6 @@ TEST_CASE("BankAccount operations", "[account]") {
 // error (the exception) occurs.
 TEST_CASE("Overdraft withdrawal throws an exception", "[account]") {
     BankAccount account(500);
-    REQUIRE(account.get_balance() == 500);
-
     // This checks that calling withdraw with smaller available balance triggers error.
     // This is the correct way to test for error conditions.
     REQUIRE_THROWS_AS(account.withdraw(510), std::runtime_error);
