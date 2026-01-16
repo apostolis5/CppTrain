50a6731e1484dc7fb1d3903812178cefab22b24a
apostolis santas <apostolis.santas@voiceland.gr>
2026-01-16 13:50:11 +0200

    clear last junk files and better build

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
 #include <iostream>
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    int result = Catch::Session().run(argc, argv);
+    if (result != 0) {
+        cout << "Tests failed!" << endl;
+        return result;
+    }
 
     cout << " ***************************Banking System Initialized*************************** " << endl;
     
-return 0;
+    return 0;
 }
